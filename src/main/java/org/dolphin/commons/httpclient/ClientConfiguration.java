cd9d6ffcac387e9ffce4d9566ef4abca588f43f4
yaokai <yaokaiabc@gmail.com>
2017-07-04 18:31:53 +0800

    httpclient4.5 连接池配置

--- src/main/java/org/dolphin/commons/httpclient/ClientConfiguration.java
+++ src/main/java/org/dolphin/commons/httpclient/ClientConfiguration.java
@@ -3,6 +3,7 @@ package org.dolphin.commons.httpclient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
@@ -31,12 +32,13 @@ public class ClientConfiguration {
             .build();
 
         try {
-            HttpGet httpget = new HttpGet("http://httpbin.org/get");
+            HttpGet httpget = new HttpGet("http://www.baidu.com");
+//        	StringEntity stringEntity = new StringEntity("post request json string","UTF-8");
             // Request configuration can be overridden at the request level.
             // They will take precedence over the one set at the client level.
             RequestConfig requestConfig = RequestConfig.copy(defaultRequestConfig)
                 .setConnectionRequestTimeout(5000)//从连接池中获取连接超时时间
-                .setConnectTimeout(5000)//创建连接超时时间
+                .setConnectTimeout(20*1000)//创建连接超时时间
                 .setSocketTimeout(5000)//等待数据超时时间
                 .build();
             httpget.setConfig(requestConfig);
@@ -45,7 +47,7 @@ public class ClientConfiguration {
             try {
                 System.out.println("----------------------------------------");
                 System.out.println(response.getStatusLine());
-                System.out.println(EntityUtils.toString(response.getEntity()));
+                System.out.println(EntityUtils.toString(response.getEntity(),"UTF-8"));
                 System.out.println("----------------------------------------");
             } finally {
                 response.close();
