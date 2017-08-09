40159d160b93b537e68f528e9539bca1e9e235b6
yaokai <yaokaiabc@gmail.com>
2017-08-09 13:55:23 +0800

    guava ording

---
+++ src/main/java/org/dolphin/commons/guava/OrderingTest.java
@@ -0,0 +1,24 @@
+package org.dolphin.commons.guava;
+
+import java.util.List;
+
+import org.apache.curator.shaded.com.google.common.collect.Lists;
+import org.apache.curator.shaded.com.google.common.collect.Ordering;
+
+public class OrderingTest {
+
+	public static void main(String[] args) {
+		List<String> list = Lists.newArrayList();
+		list.add("ABCF");
+		list.add("784-5735353389");
+		list.add("784-5735353386");
+		list.add("ABCD");
+		Ordering<String> natural = Ordering.natural();
+		list = natural.sortedCopy(list);
+		System.out.println(list);
+		for(String str:list){
+			System.out.println(str);
+		}
+	}
+
+}
