b3aa96898ae8a630a10c1156f5476c380975c33f
yaokai <yaokaiabc@gmail.com>
2018-01-29 14:53:16 +0800

    elasticsearch head插件安装

---
+++ src/test/java/org/dolphin/commons/JunitDemo.java
@@ -0,0 +1,40 @@
+package org.dolphin.commons;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class JunitDemo {
+
+	public static void main(String[] args) {
+//		Thread.currentThread().setName("my main thread dump");
+		
+		ExecutorService es = Executors.newFixedThreadPool(3);
+		
+		for(int i=0;i<5;i++){
+			es.submit(new Runnable(){
+
+				@Override
+				public void run() {
+					
+					try {
+						TimeUnit.SECONDS.sleep(20);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+				
+			});
+		}
+		
+		try {
+			TimeUnit.SECONDS.sleep(900);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+}
