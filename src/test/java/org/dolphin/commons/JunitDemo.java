55b539fac86b21d9e5346444f8910d58cbdab543
yaokai <yaokaiabc@gmail.com>
2018-01-31 15:07:58 +0800

    update

--- src/test/java/org/dolphin/commons/JunitDemo.java
+++ src/test/java/org/dolphin/commons/JunitDemo.java
@@ -7,34 +7,37 @@ import java.util.concurrent.TimeUnit;
 public class JunitDemo {
 
 	public static void main(String[] args) {
+		
+		System.out.println(2.5*4+8+2.5*2+1.5+2.5+8+2.5+1+1+1.5);
+		
 //		Thread.currentThread().setName("my main thread dump");
 		
-		ExecutorService es = Executors.newFixedThreadPool(3);
-		
-		for(int i=0;i<5;i++){
-			es.submit(new Runnable(){
-
-				@Override
-				public void run() {
-					
-					try {
-						TimeUnit.SECONDS.sleep(20);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-				}
-				
-			});
-		}
-		
-		try {
-			TimeUnit.SECONDS.sleep(900);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		ExecutorService es = Executors.newFixedThreadPool(3);
+//		
+//		for(int i=0;i<5;i++){
+//			es.submit(new Runnable(){
+//
+//				@Override
+//				public void run() {
+//					
+//					try {
+//						TimeUnit.SECONDS.sleep(20);
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//					
+//				}
+//				
+//			});
+//		}
+//		
+//		try {
+//			TimeUnit.SECONDS.sleep(900);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 	
 }
